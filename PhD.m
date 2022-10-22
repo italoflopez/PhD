82090cf41a00d79467e076b760e12c0055335ce2
italoflopez <italoflopez@yahoo.com>
2022-10-22 18:12:52 -0500

    Up to fast and slow moving factors

--- PhD.m
+++ PhD.m
@@ -21,25 +21,61 @@ end
 
 macro_data=readtable("C:\Users\Italo\Documents\PhD\PhD\standardize_macro_data.csv");
 
-sigma_x=table2array(macro_data(:,2:44))'*table2array(macro_data(:,2:44))/height(macro_data(:,2:44))
-[V,D] = eig(sigma_x)
+sigma_x=cov(table2array(macro_data(:,2:44)));
+[V,D] = eig(sigma_x);
 
-big_lambda=V(:,((length(V)-4):length(V)))
-big_lambda=big_lambda(:,[5 4 3 2 1])
+big_lambda=V(:,((length(V)-4):length(V)));
+big_lambda=big_lambda(:,[5 4 3 2 1]);
 
-ft=table2array(macro_data(:,2:44))*big_lambda/height(macro_data(:,2:44))
+ft=table2array(macro_data(:,2:44))*big_lambda/height(macro_data(:,2:44));
 
-common_component=(big_lambda*ft')'
+common_component=(big_lambda*ft')';
 
-idiosyncratic_error=table2array(macro_data(:,2:44))-common_component
+idiosyncratic_error=table2array(macro_data(:,2:44))-common_component;
 
 for i=1:43
-macro_factor_loadings(:,i)=inv(ft'*ft)*ft'*table2array(macro_data(:,(1+i)))
+macro_factor_loadings(:,i)=inv(ft'*ft)*ft'*table2array(macro_data(:,(1+i)));
 end
 
-common_component=ft*macro_factor_loadings
+common_component=ft*macro_factor_loadings;
 
-idiosyncratic_error=table2array(macro_data(:,2:44))-common_component
+idiosyncratic_error=table2array(macro_data(:,2:44))-common_component;
 
+macro_factor_loadings*macro_factor_loadings'/height(macro_data(:,2:44));
+diag(D/sum(diag(D)))*100;
 
+macro_data = [macro_data(:,1) macro_data(:,31) macro_data(:,2:30) macro_data(:,32:50)];
 
+macro_data = [macro_data(:,1:39) macro_data(:,41) macro_data(:,40) macro_data(:,42:50)];
+
+macro_data = [macro_data(:,1:40) macro_data(:,48) macro_data(:,41) macro_data(:,42:47) macro_data(:,49:50)];
+
+macro_data = [macro_data(:,1:4) macro_data(:,41) macro_data(:,5:40) macro_data(:,42:50)];
+
+macro_data = [macro_data(:,1:41) macro_data(:,49) macro_data(:,42:48) macro_data(:,50)];
+
+slow_moving_variables=table2array(macro_data(:,2:40));
+
+fast_moving_variables=table2array(macro_data(:,41:50));
+
+sigma_x_slow=cov(slow_moving_variables);
+
+sigma_x_fast=cov(fast_moving_variables);
+
+[V_slow,D_slow] = eig(sigma_x_slow);
+
+[V_fast,D_fast] = eig(sigma_x_fast);
+
+big_lambda_slow=V_slow(:,((length(V_slow)-1):length(V_slow)));
+big_lambda_slow=big_lambda_slow(:,[2 1]);
+
+big_lambda_fast=V_fast(:,((length(V_fast)-1):length(V_fast)));
+big_lambda_fast=big_lambda_fast(:,[2 1]);
+
+ft_slow=slow_moving_variables*big_lambda_slow/height(slow_moving_variables);
+
+ft_fast=fast_moving_variables*big_lambda_fast/height(fast_moving_variables);
+
+diag(D_slow/sum(diag(D_slow)))*100;
+
+diag(D_fast/sum(diag(D_fast)))*100;
