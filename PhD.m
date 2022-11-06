75158e727de0c709f971a075a337fc9db66de694
italoflopez <italoflopez@yahoo.com>
2022-11-06 10:13:28 -0600

    Up to first state equation tryout (VAR)

--- PhD.m
+++ PhD.m
@@ -1,7 +1,11 @@
 %bla
 yield_data=readtable("C:\Users\Italo\Documents\PhD\PhD\USD Zero Coupon Yields.xlsx");
 yield_data=yield_data(3:height(yield_data),:);
-yield_data=table2timetable(yield_data);
+%yield_data=table2timetable(yield_data);
+t1 = datetime(1989,3,1);
+t2 = datetime(2022,6,1);
+t = t1:calmonths(1):t2
+yield_data=table2timetable([table(datetime(t','Format','yyyyMM')), yield_data(:,2:size(yield_data,2))]);
 %macro_data=readtable("C:\Users\Italo\Documents\PhD\PhD\final_macro_data.csv");
 lambda=0.0609;
 maturity=[3, 6, 12, 24, 36, 48, 60, 72, 84, 96, 108, 120, 180, 240, 360];
@@ -14,6 +18,7 @@ ns_factor_loadings = [ones(1,length(maturity)); (1-exp(-lambda*maturity))./(lamb
 for i=1:height(yield_data);
 betas(i,:)=inv(ns_factor_loadings'*ns_factor_loadings)*ns_factor_loadings'*table2array(yield_data(i,:))';
 end
+betas=table2timetable([table(datetime(yield_data.Var1,'Format','yyyyMM')), array2table(betas)]);
 %stationarity tests
 [h,pValue,stat,cValue] = adftest(betas(:,1));
 [h,pValue,stat,cValue] = adftest(betas(:,2));
@@ -24,7 +29,7 @@ end
 [h,pValue,stat,cValue] = kpsstest(betas(:,3));
 
 macro_data=readtable("C:\Users\Italo\Documents\PhD\PhD\First\standardize_macro_data.csv");
-macro_data=table2timetable([table(datetime(char(macro_data{:,1}))), macro_data(:,2:size(macro_data,2))]);
+macro_data=table2timetable([table(datetime(macro_data.Var1,'Format','yyyyMM')), macro_data(:,2:size(macro_data,2))]);
 %Variance-covariance matrix of standardized stationary macro data
 sigma_x=cov(table2array(macro_data));
 %eigenvalue-eigenvector decomposition
@@ -77,10 +82,11 @@ big_lambda_slow=big_lambda_slow(:,[2 1]);
 
 big_lambda_fast=V_fast(:,((length(V_fast)-1):length(V_fast)));
 big_lambda_fast=big_lambda_fast(:,[2 1]);
-
+%%%%%%%
 ft_slow=table2array(slow_moving_variables)*big_lambda_slow/height(slow_moving_variables);
-
+ft_slow=table2timetable([table(datetime(slow_moving_variables.Var1,'Format','yyyyMM')), array2table(ft_slow)]);
 ft_fast=table2array(fast_moving_variables)*big_lambda_fast/height(fast_moving_variables);
+ft_fast=table2timetable([table(datetime(fast_moving_variables.Var1,'Format','yyyyMM')), array2table(ft_fast)]);
 
 diag(D_slow/sum(diag(D_slow)))*100;
 
@@ -94,9 +100,18 @@ for i=1:size(macro_data,2)
 fast_factor_loadings(:,i)=inv(ft_fast'*ft_fast)*ft_fast'*table2array(macro_data(:,i));
 end
 %slow factors with name normalization
-ft_slow_name=ft_slow*slow_factor_loadings(:,1:2);
+ft_slow_name=table2array(ft_slow)*slow_factor_loadings(:,1:2);
+ft_slow_name=table2timetable([table(datetime(slow_moving_variables.Var1,'Format','yyyyMM')), array2table(ft_slow_name)]);
 %fast factors with name normalization
-ft_fast_name=ft_fast*fast_factor_loadings(:,40:41);
+ft_fast_name=table2array(ft_fast)*fast_factor_loadings(:,40:41);
+ft_fast_name=table2timetable([table(datetime(fast_moving_variables.Var1,'Format','yyyyMM')), array2table(ft_fast_name)]);
 
 ffr=readtable("C:\Users\Italo\Documents\PhD\PhD\ffr.csv");
+ffr=table2timetable([table(datetime(char(ffr{:,1}),'Format','yyyyMM')), ffr(:,2:size(ffr,2))]);
+
+factors=synchronize(ft_slow_name,ffr,ft_fast_name,betas);
+factors=rmmissing(factors);
+
+Mdl = varm(8,2);
+EstMdl = estimate(Mdl,factors);
 
